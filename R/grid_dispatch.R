83f922fba0879db8d09ebf7783b23501f919c371
Luke Strickland <ljs392@cl2.newcastle.edu.au>
2020-12-11 18:39:22 +1100

    Add analysis of model with session effects on inhibition/excitation

--- R/grid_dispatch.R
+++ R/grid_dispatch.R
@@ -1,8 +1,8 @@
 
 source("dmc/dmc.R")
 
-load_model("LBA", "lba_B.R")
-run.grid.dmc("CA_top_samples",model.dir ="LBA",
-             model.file="lba_B.R",user="ljs392",
+load_model("LBA", "lba_B_autothres.R")
+run.grid.dmc("CA_top_thresholdsmult_samples",model.dir ="LBA",
+             model.file="lba_B_autothres.R",user="ljs392",
              n.add=60, wall.hours = 300,
-             GB = 2, max.try=30)
+             GB = 3, max.try=30)
